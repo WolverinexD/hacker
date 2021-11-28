a8df9c8cda72adb9b632e8fe03fbbae8b5ec3e6e
Hacker Jr TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-11-28 19:58:38 +0530

    Update arrow.py

--- plugins/josprojects/arrow.py
+++ plugins/josprojects/arrow.py
@@ -3,7 +3,7 @@
 
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
-from plugins.helper_func.cust_p_filters import f_onw_fliter
+from plugins.josprojects.help_func.admin_check import admin_check
 
 # EMOJI CONSTANTS
 DART_E_MOJI = "🎯"
