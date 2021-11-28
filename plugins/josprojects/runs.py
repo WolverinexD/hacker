e0291b44020746c00d35acb1256fd7150c8a05ce
Hacker Jr TG <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-11-28 19:54:59 +0530

    Update runs.py

--- plugins/josprojects/runs.py
+++ plugins/josprojects/runs.py
@@ -1,7 +1,7 @@
 import random
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import f_onw_fliter
+from plugins.helper_func.cust_p_filters import f_onw_fliter
 
 
 RUN_STRINGS = (
