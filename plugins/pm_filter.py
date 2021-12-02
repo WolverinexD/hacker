2f73486e2899d0f779e91ba358f1a2c904348734
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-12-02 07:41:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -581,7 +581,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.PIN_TXT,
+            text=Script.PIN_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )         
