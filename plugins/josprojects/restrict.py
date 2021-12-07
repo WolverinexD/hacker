0a4e93adbc7903d78ce77331daff1e87ceadd0af
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-12-07 23:21:13 +0530

    Update restrict.py

--- plugins/josprojects/restrict.py
+++ plugins/josprojects/restrict.py
@@ -5,9 +5,9 @@ from pyrogram import (
 from pyrogram.types import (
     ChatPermissions
 )
-from plugins.helper_functions.admin_check import admin_check
-from plugins.helper_functions.extract_user import extract_user
-from plugins.helper_functions.string_handling import extract_time
+from plugins.josprojects.help_func.admin_check import admin_check
+from plugins.josprojects.help_func.extract_user import extract_user
+from plugins.josprojects.help_func.string_handling import extract_time
 
 
 @Client.on_message(filters.command("mute"))
