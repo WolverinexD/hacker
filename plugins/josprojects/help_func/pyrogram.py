d0c82ffa5868412e82c0926fd0b9f6f25e3e238e
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-12-12 02:17:08 +0530

    Update pyrogram.py

--- plugins/josprojects/help_func/pyrogram.py
+++ plugins/josprojects/help_func/pyrogram.py
@@ -3,12 +3,13 @@ import logging
 from pyrogram import Client
 
 # from pyromod import listen
-from plugins.josprojects.help_func import environ
+from plugins.josprojects.help_func import get_int_key, get_str_key
 
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+
+BOT_TOKEN = get_str_key("BOT_TOKEN", required=True)
+APP_ID = get_int_key("APP_ID", required=True)
+APP_HASH = get_str_key("APP_HASH", required=True)
+session_name = BOT_TOKEN.split(":")[0]
 pbot = Client(
     session_name,
     api_id=APP_ID,
