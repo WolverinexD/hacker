e8e59c5de8a75fdb29218b1046bd0028fa8546b8
Hacker Jr <83758692+Hacker-Jr-TG@users.noreply.github.com>
2021-12-15 22:45:18 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -554,7 +554,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "afk":
         buttons = [[
-            InlineKeyboardButton('«ʙᴀᴄᴋ', callback_data='halp')
+            InlineKeyboardButton('«ʙᴀᴄᴋ', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
